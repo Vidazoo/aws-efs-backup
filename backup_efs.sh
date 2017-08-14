c98401873f2d34431d035bf8adb8ca18e2a14000
Naor Livne <naorlivne@gmail.com>
2017-08-14 16:29:01 +0300

    backup script

--- backup_efs.sh
+++ backup_efs.sh
@@ -10,5 +10,8 @@ mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=
 cd /var/tmp
 tar -zcvf $EFS_NAME.tar.gz /efs
 
+# unmount just to be on the safe side
+umount /efs
+
 # copy everything to s3
 aws s3 cp $EFS_NAME.tar.gz 3://$S3_BUCKET/efs/$EFS_NAME.`date +"%Y%m%d"`.tar.gz
