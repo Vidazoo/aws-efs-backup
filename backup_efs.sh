273a2a0cd0369a8204a357aacb695625f95c1dee
Naor Livne <naorlivne@gmail.com>
2017-08-14 14:55:01 +0300

    backup script

--- backup_efs.sh
+++ backup_efs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# stop on error
+set -e
+
 # mount the drive
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $EFS_ID.efs.us-east-1.amazonaws.com:/ /efs
 
